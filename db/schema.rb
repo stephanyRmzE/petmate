6bb43945b4e4251e844419c902882ee827427d55
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-02-21 15:35:15 +0100

    created controllers

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,8 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2022_02_18_152446) do
+
+ActiveRecord::Schema.define(version: 2022_02_21_134929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
