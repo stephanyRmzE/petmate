aceb9653f7364eb04a0566aa342aad9cb2c046e8
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-02-21 17:36:53 +0100

    added basic views to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,11 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :destroy]
   def show
+    @user = User.find(params[:id])
   end
 
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
     redirect_to root_path
-  end
-
-  private
-  def set_user
-    @user = User.find(params[:id])
   end
 end
