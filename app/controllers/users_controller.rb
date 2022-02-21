47a41aba75ccce8a0e9d3e4d28ad86203a47aca4
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-02-21 16:54:08 +0100

    finished pet controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :destroy]
+  def show
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
