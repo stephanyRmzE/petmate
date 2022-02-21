4d03080fac4e1d1bf1cf27389a0f69efdb33145b
stephany ramirez <stephany_re@hotmail.com>
2022-02-21 07:19:12 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :user, only: [:show, :destroy]
+  resources :pets, only: [:create, :new, :destroy, :index, :show] do
+    resources :reservations, only: [:new, :create, :index, :show, :destroy]
+  end
 end
