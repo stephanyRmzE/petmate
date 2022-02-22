742fea27403e2c42ef51bc5fb45e7308ea73e706
stephany ramirez <stephany_re@hotmail.com>
2022-02-22 09:49:57 -0700

    reservation form

---
+++ app/policies/reservation_policy.rb
@@ -0,0 +1,30 @@
+class ReservationPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    record.id == user.id
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+
+  def destroy?
+    record.id == user.id
+  end
+end
