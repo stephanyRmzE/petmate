88a95712b97e73b1b899a6336161e504a7bd1c52
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-02-22 12:06:58 +0100

    adding changes to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
     redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:usrt).permit(:first_name, :last_name, :photo)
   end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,5 @@
 class Pet < ApplicationRecord
   has_many :reservations
+  belongs_to :user
   has_one_attached :photo
 end
