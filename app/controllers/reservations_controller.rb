f42e30b2a5374dd980638af60e9bdc0a3e01ea55
Astrid de Jaegher <astriddejaegher97@gmail.com>
2022-02-22 09:57:34 +0100

    reservations controller + login

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,39 @@
 class ReservationsController < ApplicationController
+  before_action :set_reservation, only: [:show, :destroy]
+  def index
+    @reservation = Reservation.all
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @user = current_user
+    @reservation.user = @user
+    if @reservation.save
+      redirect_to reservations_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @reservation.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :user, :pet)
+  end
 end
