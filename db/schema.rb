601d6d556d0761e89c96afbf21b5f3299433445c
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-02-24 11:30:53 +0100

    Create enhanced seeds and add migration for pet description, took away filter on all photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_111902) do
+ActiveRecord::Schema.define(version: 2022_02_24_101523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2022_02_23_111902) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
